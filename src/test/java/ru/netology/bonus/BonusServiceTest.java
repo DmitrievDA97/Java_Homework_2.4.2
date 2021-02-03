607b63e392fd18917a65a297ed899b8a9f9946f5
Denis Dmitriev <denitos97@gmail.com>
2021-02-03 16:31:43 +0300

    Added two tests

--- src/test/java/ru/netology/bonus/BonusServiceTest.java
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -1,18 +1,15 @@
 package ru.netology.bonus;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BonusServiceTest {
-  @ParameterizedTest
-  @CsvSource(
-      value={
-          "'registered user, bonus under limit',100060,true,30",
-          "'registered user, bonus over limit',100000060,true,500"
-      }
-  )
+  @ParameterizedTest(name = "{0}")
+  @CsvFileSource(resources = "/data.csv")
+
   void shouldCalculate(String test, long amount, boolean registered, long expected) {
     BonusService service = new BonusService();
 
